f9a1bb7562445f304e7134f0a026990e88f2d580
ErrorOliver <ErrorOliver6@proton.me>
2025-05-18 05:44:17 +0000

    Update logger.ts

--- src/renderer/logger.ts
+++ src/renderer/logger.ts
@@ -1,9 +1,9 @@
 /*
- * Vesktop, a desktop app aiming to give you a snappier Discord Experience
- * Copyright (c) 2025 Vendicated and Vencord contributors
+ * Scorntop, a desktop app aiming to give you a snappier Discord Experience
+ * Copyright (c) 2025 Scorncord contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import { Logger } from "@vencord/types/utils";
 
-export const VesktopLogger = new Logger("Vesktop", "#d3869b");
+export const VesktopLogger = new Logger("Scorntop "#d3869b");
